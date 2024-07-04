65e2f8dd6d7e424a5ee03cb0b2ad75ff40c0dd99
Cringy <corbomatteo08@gmail.com>
2024-07-04 15:53:02 +0200

    Corrections to Code of Projects page

--- Esame/projects.php
+++ Esame/projects.php
@@ -6,53 +6,54 @@ $data = strumenti::leggiJSON("json/data.json", true)["projects"];
 exit; */
 ?>
 
+<!DOCTYPE html>
 <html lang="en">
     <!-- Head incluso da inclusioni/head.php -->
     <?php require_once("inclusioni/head.php") ?>
     <body>
         <!-- Barra di Navigazione -->
         <?php require_once("inclusioni/navbar.php") ?>
-    </body>
-    <main>
-        <h1>Works</h1>
-        <!-- Bolle di sfondo -->
-        <div class="bubbles">
-            <?php for ($i = 0; $i < $data['bubbles']; $i++) { ?>
-                <div class="bubble"></div>    
-            <?php } ?>
-        </div>
-        <!-- Lavori -->
-        <div id="works">
-            <?php 
-                $loopCounter = 0;
-                foreach ($data['project_list'] as $project) {
-            ?>
-            <!-- Sezione di Un lavoro -->
-            <div class="singleProject">
-                <!-- Immagine -->
-                <div class="cards <?php echo $loopCounter % 2 == 0 ? "left" : "right" ?>">
-                    <a href="<?php echo $project['project_page'] ?>" title="<?php echo $project['image']['title'] ?>">
-                        <img src="<?php echo $project['image']['link'] ?>" alt="<?php echo $project['image']['alt_text'] ?>">
-                    </a>
-                </div>
-                <!-- Testo -->
-                <div class="projectDescription">
-                    <!-- Titolo del Progetto -->
-                    <div class="descriptionTitle">
-                        <p><?php echo $project['project_title'] ?></p>
-                    </div>
-                    <!-- Descrizione del Progetto -->
-                    <div class="descriptionText">
-                        <p><?php echo $project['project_description'] ?></p>
-                    </div>
-                </div>
+        <main>
+            <h1>Works</h1>
+            <!-- Bolle di sfondo -->
+            <div class="bubbles">
+                <?php for ($i = 0; $i < $data['bubbles']; $i++) { ?>
+                    <div class="bubble"></div>    
+                <?php } ?>
             </div>
-            <?php
-                $loopCounter++;
-                } 
-            ?>
-        </div>
-    </main>
-    <!-- Footer incluso da footer.php -->
-    <?php require_once("inclusioni/footer.php") ?>
+            <!-- Lavori -->
+            <div id="works">
+                <?php 
+                    $loopCounter = 0;
+                    foreach ($data['project_list'] as $project) {
+                ?>
+                <!-- Sezione di Un lavoro -->
+                <div class="singleProject">
+                    <!-- Immagine -->
+                    <div class="cards <?php echo $loopCounter % 2 == 0 ? "left" : "right" ?>">
+                        <a href="<?php echo $project['project_page'] ?>" title="<?php echo $project['image']['title'] ?>">
+                            <img src="<?php echo $project['image']['link'] ?>" alt="<?php echo $project['image']['alt_text'] ?>">
+                        </a>
+                    </div>
+                    <!-- Testo -->
+                    <div class="projectDescription">
+                        <!-- Titolo del Progetto -->
+                        <div class="descriptionTitle">
+                            <p><?php echo $project['project_title'] ?></p>
+                        </div>
+                        <!-- Descrizione del Progetto -->
+                        <div class="descriptionText">
+                            <p><?php echo $project['project_description'] ?></p>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                    $loopCounter++;
+                    } 
+                ?>
+            </div>
+        </main>
+        <!-- Footer incluso da footer.php -->
+        <?php require_once("inclusioni/footer.php") ?>
+    </body>
 </html>
