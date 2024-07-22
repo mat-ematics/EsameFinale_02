072361c7e2ded1384f461b9f4640672d4f0829a9
Cringy <corbomatteo08@gmail.com>
2024-07-22 12:34:17 +0200

    Fixed Link Paths for New Root

--- inclusioni/head.php
+++ inclusioni/head.php
@@ -7,7 +7,7 @@ use assets\strumenti;
     <!-- Titolo pagina Home -->
     <title><?php echo $data['page_title'] ?></title>
     <!-- Font e icone varie -->
-    <link rel="stylesheet" type="text/css" href="/css/<?php echo $data['style_sheet'] ?>">
+    <link rel="stylesheet" type="text/css" href="css/<?php echo $data['style_sheet'] ?>">
     <script src="https://kit.fontawesome.com/81cee0c499.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">    
     <link rel="preconnect" href="https://fonts.googleapis.com">
