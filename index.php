bba0c768e599d4f954afae6b9f141e4507697ce4
Cringy <corbomatteo08@gmail.com>
2024-07-23 21:26:47 +0200

    Added Images instead of grey placeholders

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once("inclusioni/strumenti.php");
 use assets\strumenti;
 $data = strumenti::leggiJSON("json/data.json", true)["index"];
+$projects = strumenti::leggiJSON("json/data.json", true)['projects']['project_list'];
 /* strumenti::stampaArray($data);
 exit; */
 ?>
@@ -44,16 +45,20 @@ exit; */
         <h1 class="title" id="workSection"><?php echo $data['work_section']['section_title'] ?></h1>
         <div id="worksWrapper">
             <div class="works">
-                <?php foreach ($data['work_section']['cards'] as $card) { ?>
+                <?php $project_limit = $data['work_section']['project_count'];
+                      $counter = 0; ?>
+                <?php foreach ($projects as $project) { ?>
+                    <?php if ($counter >= $project_limit) break; ?>
                     <!-- Singola Card progetto -->
                     <div class="cards">
-                        <img src="<?php echo $card['card_image']['link'] ?>" alt="<?php echo $card['card_image']['alt_text'] ?>">
+                        <img src="<?php echo $project['image']['link'] ?>" alt="<?php echo $project['image']['alt_text'] ?>" title="<?php echo $project['image']['title'] ?>">
                         <!-- Overlay-on-hover -->
                         <div class="overlay">
-                            <h2><?php echo $card['card_title'] ?></h2>
-                            <p><?php echo $card['card_description'] ?></p>
+                            <h2><?php echo $project['project_title'] ?></h2>
+                            <p><?php echo $project['project_summary'] ?></p>
                         </div>
                     </div>
+                    <?php $counter++; ?>
                 <?php } ?>
             </div>
         </div>
