2d5750a0b42ded4e16b89d64a526ce265f994cd4
Cringy <corbomatteo08@gmail.com>
2024-07-24 14:42:45 +0200

    Created global common standard stylesheet for redundant styles

--- inclusioni/head.php
+++ inclusioni/head.php
@@ -7,6 +7,7 @@ use assets\strumenti;
     <!-- Titolo pagina Home -->
     <title><?php echo $data['page_title'] ?></title>
     <!-- Font e icone varie -->
+    <link rel="stylesheet" type="text/css" href="css/global.min.css">
     <link rel="stylesheet" type="text/css" href="css/<?php echo $data['style_sheet'] ?>">
     <script src="https://kit.fontawesome.com/81cee0c499.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">    
