db3696535e212a56279cb1309f58d8b571a6ece2
Cringy <corbomatteo08@gmail.com>
2024-07-26 16:45:31 +0200

    Added Content and Modifed Titles

--- inclusioni/head.php
+++ inclusioni/head.php
@@ -14,6 +14,8 @@ use assets\strumenti;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&family=Source+Code+Pro:ital,wght@0,600;1,600&display=swap" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="images/logo.ico">
     <!-- Adattatore schermo -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
